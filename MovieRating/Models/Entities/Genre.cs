7b267dc5cba679c89feb36c9e74823e6dd5525e8
DZM77 <dimitris.bjorlingh@outlook.com>
2022-07-04 13:35:26 +0200

    Movie, Genre

---
+++ MovieRating/Models/Entities/Genre.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieRating.Models.Entities
+{
+    public enum Genre
+    {
+        Horror,
+        Thriller,
+        Comedy,
+        Drama,
+        Action
+    }
+}
---
+++ MovieRating/Models/Entities/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieRating.Models.Entities
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        public string? Title { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        [Range(1,5)]
+        public float Rating { get; set; }
+
+    }
+}
