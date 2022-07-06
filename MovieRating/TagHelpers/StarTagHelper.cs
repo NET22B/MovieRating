0a784940e0db6a8391a37244d465a30eb171503f
DZM77 <dimitris.bjorlingh@outlook.com>
2022-07-06 13:43:24 +0200

    TagHelper

---
+++ MovieRating/TagHelpers/StarTagHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text;
+using System.Text.Encodings.Web;
+
+namespace MovieRating.TagHelpers
+{
+    [HtmlTargetElement("star")]
+    public class StarTagHelper : TagHelper
+    {
+        public float Rating { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "span";
+            output.AddClass("star", HtmlEncoder.Default);
+
+            var doubleRating = (int)Math.Round(Rating * 2);
+
+            var stars = doubleRating / 2;
+            var isHalfStar = doubleRating % 2 == 1;
+
+            var commons = "https://upload.wikimedia.org/wikipedia/commons/";
+            var starImg = commons + "e/e5/Full_Star_Yellow.svg";
+            var halfImg = commons + "d/d6/Half_Star_Yellow.svg";
+
+            var builder = new StringBuilder();
+
+            for (int i = 0; i < stars; i++)
+            {
+                builder.Append($"<img src='{starImg}'/>");
+            }
+            if (isHalfStar) builder.Append($"<img src='{halfImg}'/>");
+
+            output.Content.SetHtmlContent(builder.ToString());
+
+        }
+    }
+}
