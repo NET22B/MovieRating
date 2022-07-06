d13ac08bcab625d9f0f7c2d3c7262a7c1d405de7
DZM77 <dimitris.bjorlingh@outlook.com>
2022-07-06 11:57:53 +0200

    ViewComponent

---
+++ MovieRating/Models/ViewModels/StarViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieRating.Models.ViewModels
+{
+    public class StarViewModel
+    {
+        public int Stars { get; set; }
+        public bool IsHalfStar { get; set; }
+    }
+}
---
+++ MovieRating/ViewComponents/StarViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieRating.Data;
+using MovieRating.Models.ViewModels;
+
+namespace MovieRating.ViewComponents
+{
+    public class StarViewComponent : ViewComponent
+    {
+        //private readonly MovieRatingContext context;
+
+        //public StarViewComponent(MovieRatingContext context)
+        //{
+        //    this.context = context;
+        //}
+
+        public IViewComponentResult Invoke(float rating)
+        {
+            var doubleRating = (int)Math.Round(rating * 2);
+
+            var model = new StarViewModel
+            {
+                Stars = doubleRating / 2,
+                IsHalfStar = doubleRating % 2 == 1,
+            };
+
+            return View(model);
+        }
+    }
+}
